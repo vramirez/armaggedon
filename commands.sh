eadffbe865394c5fc0d40ce06064c37c52f1a6db
Victor Ramirez <ramirez.vmanuel@gmail.com>
2018-12-22 20:46:46 -0500

    Create commands.sh

---
+++ commands.sh
@@ -0,0 +1,11 @@
+# Extraido con `history | cut -c 8-`
+sudo snap install libreoffice
+sudo snap install atom --classic
+sudo snap install evince
+sudo snap install calibre
+sudo snap install gedit
+sudo snap install spotify
+sudo snap install vscode  --classic
+
+#git, microcode Inte, cmd line calc, gnome visual for tweaking panels
+sudo apt install gnome-tweak-tool intel-microcode git apcalc
